c2ee5a4c7d7c33c7b13b9f365ce3249393671f29
Jonathan-Lynch <jonathanlynch2001@gmail.com>
2020-07-28 11:33:53 -0500

    Add project files.

---
+++ JonathanGraduationBookApp.Core/Model/Author.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace JonathanGraduationBookApp.Core.Model
+{
+	public class Author
+	{
+		public int Id { get; set; }
+		public string AuthorName { get; set; }
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+		public ICollection<Book> Books { get; set; }
+	}
+}
---
+++ JonathanGraduationBookApp.Core/Model/Book.cs
@@ -0,0 +1,13 @@
+﻿
+namespace JonathanGraduationBookApp.Core.Model
+{
+	public class Book
+	{
+		public int Id { get; set; }
+		public string Title { get; set; }
+		public string Genre { get; set; }
+		public int AuthorId { get; set; }
+		public Author Author { get; set; }
+
+	}
+}
