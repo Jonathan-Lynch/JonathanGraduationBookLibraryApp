51467aa2eb1646abe3057451534e64d5764ab469
Jonathan-Lynch <jonathanlynch2001@gmail.com>
2020-07-28 12:16:45 -0500

    Finished Controllers Folders

---
+++ JonathanGraduationBookApp/Controllers/AuthorController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using JonathanGraduationBookApp.Core.Services;
+using Microsoft.AspNetCore.Mvc;
+using JonathanGraduationBookApp.ApiModels;
+using JonathanGraduationBookApp.Core.Model;
+
+namespace JonathanGraduationBookApp.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class AuthorController : ControllerBase
+	{
+		private readonly IAuthorServices _authorServices;
+
+		public AuthorController(IAuthorServices authorServices)
+		{
+			_authorServices = authorServices;
+		}
+
+		//get api/authors
+		[HttpGet]
+		public IActionResult Get()
+		{
+			var authorModel = _authorServices.GetAll().ToApiModels();
+			return Ok(authorModel);
+		}
+
+		//get api/authors/:id
+		[HttpGet("{id}")]
+		public IActionResult Get(int id)
+		{
+			var author = _authorServices.Get(id).ToApiModel();
+			if (author == null) return NotFound();
+			return Ok(author);
+		}
+
+		//Post api/authors/:id
+		[HttpPost]
+		public IActionResult Post([FromBody] AuthorModel newAuthor)
+		{
+			try
+			{
+				_authorServices.Add(newAuthor.ToDomainModel());
+			}
+			catch (System.Exception ex)
+			{
+				ModelState.AddModelError("addAuthor", ex.GetBaseException().Message);
+			}
+			return CreatedAtAction("Get", new { Id = newAuthor.Id }, newAuthor);
+		}
+
+		//put api/books/:id
+		[HttpPut("{id}")]
+		public IActionResult Put(int id, [FromBody] AuthorModel updatedAuthor)
+		{
+			var author = _authorServices.Update(updatedAuthor.ToDomainModel());
+			if (author == null) return NotFound();
+			return Ok(author.ToApiModel());
+		}
+
+		[HttpDelete("{id}")]
+		public IActionResult Delete(int id)
+		{
+			var author = _authorServices.Get(id);
+			if (author == null) return NotFound();
+			_authorServices.Remove(id);
+			return NoContent();
+		}
+	}
+}
---
+++ JonathanGraduationBookApp/Controllers/BookController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using JonathanGraduationBookApp.ApiModels;
+using JonathanGraduationBookApp.Core.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JonathanGraduationBookApp.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class BookController : ControllerBase
+	{
+		private readonly IBookServices _bookServices;
+
+		public BookController(IBookServices bookServices)
+		{
+			_bookServices = bookServices;
+		}
+
+		// get api/books
+		[HttpGet]
+		public IActionResult Get()
+		{
+			var bookModels = _bookServices.GetAll().ToApiModels();
+			return Ok(bookModels);
+		}
+
+
+		//get api/books/:id
+		[HttpGet("{id}")]
+		public IActionResult Get(int id)
+		{
+			var book = _bookServices.Get(id).ToApiModel();
+			if (book == null) return NotFound();
+			return Ok(book);
+		}
+
+		//Post api/books/:id
+		[HttpPost]
+		public IActionResult Post([FromBody] BookModel newBook)
+		{
+			try
+			{
+				_bookServices.Add(newBook.ToDomainModel());
+			}
+			catch (System.Exception ex)
+			{
+				ModelState.AddModelError("addBook", ex.GetBaseException().Message);
+				return BadRequest(ModelState);
+			}
+			return CreatedAtAction("Get", new { Id = newBook.Id }, newBook);
+		}
+
+		//put api/books/:id
+		[HttpPut("{id}")]
+		public IActionResult Put(int id, [FromBody] BookModel updatedBook)
+		{
+			var book = _bookServices.Update(updatedBook.ToDomainModel());
+			if (book == null) return NotFound();
+			return Ok(book.ToApiModel());
+		}
+
+		[HttpDelete("{id}")]
+		public IActionResult Delete(int id)
+		{
+			var book = _bookServices.Get(id);
+			if (book == null) return NotFound();
+			_bookServices.Remove(id);
+			return NoContent();
+		}
+	}
+}
